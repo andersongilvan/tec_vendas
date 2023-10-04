86f0db3da39e0a6c78a4e89e5c37bed7a7c355fa
andersongilvan <133284440+andersongilvan@users.noreply.github.com>
2023-10-04 16:14:11 -0300

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tec Vendas - Sua Loja de Tecnologia</title>
+    <link rel="stylesheet" href="estilo.css">
+</head>
+<body>
+    <header>
+        <h1>Tec Vendas</h1>
+        <p>Sua Loja de Tecnologia</p>
+        <h2>Conheça mais sobre a Tec Vendas e nossa missão de oferecer tecnologia de qualidade.</h2>
+    </header>
+
+    <nav>
+        <ul>
+            <li><a href="home">Home</a></li>
+            <li><a href="/produtos">Produtos</a></li>
+
+
+            <h2>Sobre nós</h2>
+            <p>A Tec Vendas, nossa loja de tecnologia fictícia, é o seu destino ideal para encontrar as mais recentes inovações tecnológicas e produtos de alta qualidade. Com um compromisso inabalável com a excelência e a satisfação do cliente, a Tec Vendas se destaca como uma referência no mundo das compras de eletrônicos e gadgets.
+
+                Nossa ampla seleção de produtos abrange desde smartphones e tablets até laptops de última geração, acessórios inteligentes e eletrônicos para o lar. Trabalhamos com as principais marcas do mercado, garantindo que nossos clientes tenham acesso aos produtos mais confiáveis e inovadores disponíveis.
+                
+                Na Tec Vendas, não se trata apenas de vender produtos, mas de fornecer uma experiência de compra excepcional. Nossa equipe dedicada de especialistas em tecnologia está sempre à disposição para oferecer orientações e recomendações personalizadas, ajudando você a tomar decisões informadas. Além disso, nossa loja online oferece conveniência e acessibilidade, tornando a compra de tecnologia mais simples do que nunca.
+                
+                Valorizamos a confiança dos nossos clientes e garantimos qualidade, preços competitivos e um atendimento excepcional. Seja você um entusiasta da tecnologia ou alguém em busca do produto perfeito, a Tec Vendas está pronta para atender às suas necessidades. Venha nos visitar virtualmente e descubra o futuro da tecnologia na Tec Vendas.
+                
+                
+                
+                
+                
+                
+                </p>
+            
+        </ul>
+    </nav>
+
+    <main>
+        <section class="featured-products">
+            
+            <!-- Insira produtos em destaque aqui -->
+        </section>
+
+        <section class="about-us">
+           
+            
+        </section>
+    </main>
+
+    <footer>
+        <div class="container">
+            <p>&copy; 2023 Tec Vendas - Todos os direitos reservados.</p>
+            <ul>
+                <li><a href="#">Política de Privacidade</a></li>
+                <li><a href="#">Termos de Uso</a></li>
+                <li><a href="#">Contato</a></li>
+            </ul>
+        </div>
+    </footer>
+</body>
+</html>
---
+++ resources/views/tec_vendas.blade.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tec Vendas - Sua Loja de Tecnologia</title>
+    <style>
+        /* Estilos CSS para personalização */
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f5f5f5;
+        }
+
+        header {
+            background-color: #333;
+            color: #fff;
+            text-align: center;
+            padding: 20px 0;
+        }
+
+        h1 {
+            font-size: 36px;
+        }
+
+        .container {
+            max-width: 1200px;
+            margin: 0 auto;
+            padding: 20px;
+        }
+
+        .product {
+            background-color: #fff;
+            border: 1px solid #ddd;
+            padding: 20px;
+            margin-bottom: 20px;
+        }
+
+        .product img {
+            max-width: 100%;
+            height: auto;
+        }
+
+        .product h2 {
+            font-size: 24px;
+            margin: 10px 0;
+        }
+
+        .product p {
+            font-size: 16px;
+            margin: 10px 0;
+        }
+
+        .product .price {
+            font-size: 20px;
+            color: #e44d26;
+            margin: 10px 0;
+        }
+
+        .btn-buy {
+            background-color: #e44d26;
+            color: #fff;
+            padding: 10px 20px;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 18px;
+            border-radius: 5px;
+        }
+
+        .btn-buy:hover {
+            background-color: #ff6f42;
+        }
+
+    </style>
+</head>
+<body>
+    <header>
+        <h1>Tec Vendas</h1>
+        <p>Sua Loja de Tecnologia</p>
+    </header>
+
+    <div class="container">
+        <div class="product">
+            <img src="\imagens\samsung-book-white-82png_20210319095216.webp"width="500px" height="200px" alt="Produto 1">
+            <h2>Notebook Samsung Book Intel®, Windows 11 Home, 4GB, 256GB SSD, 15.6'' Full HD LED Cinza Chumbo</h2>
+            <p>O Notebook Samsung Book é um laptop Windows 11 Home com processador Intel, 4GB de RAM e 256GB de armazenamento SSD. Possui uma tela Full HD LED de 15,6 polegadas e um design elegante em cinza. É uma opção prática para tarefas diárias de produtividade e entretenimento.</p>
+            <p class="price">R$ 3.400</p>
+            <a href="#" class="btn-buy">Comprar</a>
+        </div>
+
+        <div class="product">
+            <img src="\imagens\imagem2.jpg" alt="Produto 2">
+            <h2>Notebook Lenovo IdeaPad 3i 82BSS00500 Intel Core i7 10510U 15,6" 8GB SSD 256 GB Linux GeForce MX330 </h2>
+            <p>O Notebook Lenovo IdeaPad 3i 82BSS00500 é um laptop versátil com um processador Intel Core i7 10510U, tela de 15,6 polegadas, 8GB de RAM, SSD de 256 GB e executa o sistema operacional Linux. É uma escolha sólida para usuários que buscam desempenho e flexibilidade, incluindo uma placa de vídeo GeForce MX330 para tarefas gráficas.</p>
+            <p class="price">R$ 3.123</p>
+            <a href="#" class="btn-buy">Comprar</a>
+        </div>
+        <div class="container">
+            <div class="product">
+                <img src="\imagens\teclado-usb.jpg"width="500px" height="400px" alt="Produto 1">
+                <h2>TECLADO USB GAMER HP K110 PRETO</h2>
+                <p>O Teclado USB Gamer HP K110, na cor preta, é um teclado projetado para atender às necessidades de jogadores. Oferece teclas responsivas e retroiluminação, proporcionando uma experiência de jogo confortável e imersiva. É uma escolha sólida para jogadores que procuram um teclado com desempenho e estilo.</p>
+                <p class="price">R$ 139,90</p>
+                <a href="#" class="btn-buy">Comprar</a>
+            </div>
+            <div class="product">
+                <img src="\imagens\mouse-gamer.jpg"width="200px" height="400px" alt="Produto 1">
+                <h2>Mouse gamer 3200 dpi com fio USB 7D extreme 7cores RGB xtreme 4modo DPI gaming MS-G260 x7</p>
+                <p class="price">R$ 100,00</p>
+                <a href="#" class="btn-buy">Comprar</a>
+            </div>
+
+            <div class="product">
+                <img src="\imagens\mouse-gamer.jpg"width="200px" height="400px" alt="Produto 1">
+                <h2>Mouse gamer 3200 dpi com fio USB 7D extreme 7cores RGB xtreme 4modo DPI gaming MS-G260 x7</p>
+                <p class="price">R$ 100,00</p>
+                <a href="#" class="btn-buy">Comprar</a>
+            </div>
+            
+            <div class="product">
+                <img src="\imagens\mouse-branco.jpeg"width="300px" height="400px" alt="Produto 1">
+                <h2>Darmoshark Mouse Para Jogos Sem Fio M3 4Khz Tri-Mode 2.4G/Usb-C/Bluetooth 26K DPI Sensor Óptico Paw3395 Peso Leve De 58G 5 Botões Programáveis Duração Bateria 800Mah Nordic 52840 Para Pc Laptop Branco</h2>
+                <p class="price">R$ 100,00</p>
+                <a href="#" class="btn-buy">Comprar</a>
+            </div>
+                <a href="home" class="button">Início</a>
+        
+        <!-- Adicione mais produtos conforme necessário -->
+    </div>
+</body>
+</html>
